ceffa445712c218ade0002c2e7affb9479623602
Iñigo Diaz Iribarnegaray <inigo.diaz.iribarnegaray@gmail.com>
2025-06-12 11:04:28 +0200

    Full basic minimax

--- src/computer.rs
+++ src/computer.rs
@@ -2,12 +2,41 @@ use crate::board::{Board, Move, Player};
 
 use std::cmp::Ordering;
 
+pub fn minmax(board: &Board) -> Option<Move> {
+    let player = board.player?;
+    assert_eq!(player, Player::Black);
+
+    board.moves().into_iter().max_by_key(|m| {
+        helper(&board.play(m).unwrap(), player, 3)
+    })
+}
+
+fn helper(board: &Board, player: Player, depth: u8) -> i16 {
+    if depth == 0 || board.player.is_none() {
+        return heuristic(board, player);
+    }
+
+    let current_player = board.player.unwrap();
+    let maximize = current_player == player;
+    println!("{}", maximize);
+
+    let branches = board.moves().into_iter().map(|m| {
+        helper(&board.play(&m).unwrap(), player, depth - 1)
+    });
+
+    if maximize {
+        branches.max()
+    } else {
+        branches.min()
+    }.unwrap()
+}
+
 fn heuristic(board: &Board, player: Player) -> i16 {
     let (b, w) = board.score();
     let rel_score: i16 = match player {
-        Player::Black => b - w,
-        Player::White => w - b,
-    }.into();
+        Player::Black => b as i16 - w as i16,
+        Player::White => w as i16 - b as i16,
+    };
 
     if board.player.is_none() {
         match rel_score.cmp(&0) {
@@ -18,33 +47,4 @@ fn heuristic(board: &Board, player: Player) -> i16 {
     } else {
         rel_score
     }
-}
-
-pub fn minmax(board: &Board) -> Option<Move> {
-    let player = board.player?;
-
-    board.moves().into_iter().max_by_key(|m| {
-        helper(&board.play(m).unwrap(), 3, player)
-    })
-}
-
-fn helper(board: &Board, depth: u8, player: Player) -> i16 {
-    let current_player = board.player.unwrap();
-    let _maximize = current_player == player;
-
-    if depth == 0 {
-        board.moves().into_iter().max_by_key(|m| {
-            let (b, w) = board.play(m).unwrap().score();
-            match player {
-                Player::Black => b - w,
-                Player::White => w - b,
-            }
-        });
-    }
-
-    board.moves().into_iter().max_by_key(|m| {
-        helper(&board.play(m).unwrap(), depth - 1, player)
-    });
-    
-    todo!()
 }
