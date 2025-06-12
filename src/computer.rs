3c1afe94fb46d5b9bb238bfd694a0096a7df65d4
Iñigo Diaz Iribarnegaray <inigo.diaz-iribarnegaray@etu.u-bordeaux.fr>
2025-06-12 15:11:49 +0200

    Rudimentary alpha-beta

--- src/computer.rs
+++ src/computer.rs
@@ -1,9 +1,20 @@
 use crate::board::{Board, Move, Player};
 
+use std::cmp;
 use std::cmp::{Ordering, Ord};
 use rand::{rng, seq::SliceRandom};
 
 type Heuristic<T> = fn(&Board, &Player) -> T;
+
+pub trait Bounds {
+    const MIN: Self;
+    const MAX: Self;
+}
+
+impl Bounds for i16 {
+    const MIN: i16 = i16::MIN;
+    const MAX: i16 = i16::MAX;
+}
 
 pub fn minmax(board: &Board) -> Option<Move> {
     let player = board.player?;
@@ -32,6 +43,48 @@ fn helper<T: Ord>(board: &Board, player: &Player, depth: u8, heuristic: Heuristi
     } else {
         branches.min()
     }.unwrap()
+}
+
+pub fn ab_minmax<T: Ord + Bounds + Copy>(board: &Board, heuristic: Heuristic<T>) -> Option<Move> {
+    let player = board.player?;
+
+    let mut moves = board.moves();
+    moves.shuffle(&mut rng());
+    moves.into_iter().max_by_key(|m| {
+        ab_helper(&board.play(m).unwrap(), &player, 3, T::MIN, T::MAX, heuristic)
+    })
+}
+
+fn ab_helper<T: Ord + Bounds + Copy>(board: &Board, player: &Player, depth: u8, mut alpha: T, mut beta: T, heuristic: Heuristic<T>) -> T {
+    if depth == 0 || board.player.is_none() {
+        return heuristic(board, player);
+    }
+
+    let current_player = board.player.unwrap();
+    let maximize = current_player == *player;
+
+    let mut optimal_eval = if maximize { T::MAX } else { T::MIN };
+    for m in board.moves() {
+        let eval = ab_helper(&board.play(&m).unwrap(), player, depth - 1, alpha, beta, heuristic);
+
+        if eval == optimal_eval {
+            continue;
+        }
+
+        if maximize {
+            optimal_eval = cmp::max(eval, optimal_eval);
+            alpha = cmp::max(optimal_eval, alpha);
+        } else {
+            optimal_eval = cmp::min(eval, optimal_eval);
+            beta = cmp::min(optimal_eval, beta);
+        }
+
+        if alpha >= beta {
+            break;
+        }
+    }
+
+    optimal_eval
 }
 
 fn heuristic(board: &Board, player: &Player) -> i16 {
